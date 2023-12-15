13305cffad535b8281f6a1ea917cb00e25e903c4
poescal1 <153814747+poescal1@users.noreply.github.com>
2023-12-15 13:01:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Voeg een klasse toe aan de navigatie bij scrollen
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  if (window.scrollY > 100) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
