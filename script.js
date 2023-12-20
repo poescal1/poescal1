b6c0b00c0bd64cd9a5090c0ee2eda85f5c97f40f
poescal1 <153814747+poescal1@users.noreply.github.com>
2023-12-20 14:23:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-// Voeg een klasse toe aan de navigatie bij scrollen
-window.addEventListener('scroll', function() {
-  const header = document.querySelector('header');
-  if (window.scrollY > 100) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
+document.addEventListener("DOMContentLoaded", function () {
+  const addProjectButton = document.getElementById("add-project");
+  const projectsList = document.querySelector(".projects-list");
+
+  // Bestaande functionaliteit om op 'Add Project' te klikken en een nieuw project toe te voegen
+  addProjectButton.addEventListener("click", function () {
+    const newProject = document.createElement("div");
+    newProject.classList.add("project");
+
+    newProject.innerHTML = `
+      <h3><a href="#">New Project</a></h3>
+      <p>Description of the new project...</p>
+    `;
+
+    projectsList.appendChild(newProject);
+  });
+
+  // Voeg hieronder de bestaande JavaScript-functionaliteit toe voor andere delen van de website
+  // ...
+
 });
